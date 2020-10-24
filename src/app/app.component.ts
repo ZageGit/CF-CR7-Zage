e017eddd948e3a3abd5e3047f3e85ad0ee2fb432
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-24 02:19:14 +0200

    background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'travelApp';
 }
+window.onload = function () {
+  var backgroundImg=[ "../assets/mountain.jpg",
+                      "../assets/beach.jpg",
+                      "../assets/boat.jpg",
+                      "../assets/venice.jpg",
+                      "../assets/jungle.jpg",
+                      "../assets/sunrise.jpg",
+  ]
+  var base = "../assets/background.jpg"
+  document.body.style.backgroundImage = "url('../assets/background.jpg')";
+  backgroundImg.forEach(function(img){
+    new Image().src = base + img; 
+
+    setInterval(changeImage, 5000);
+   function changeImage() {   
+    var i = Math.floor((Math.random() * 2));
+    
+    document.body.style.backgroundImage = "url('"+backgroundImg[i]+"')";
+    
+  }
+})}
